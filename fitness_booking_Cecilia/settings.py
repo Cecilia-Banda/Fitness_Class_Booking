59753f407c04523ec6f235912261507b89ce6f88
Cecilia-Banda <c.banda@alustudent.com>
2025-03-29 15:37:05 +0200

    authentication

--- fitness_booking_Cecilia/settings.py
+++ fitness_booking_Cecilia/settings.py
@@ -37,10 +37,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-
-    # Custom apps
-    'accounts',
-    'bookings',
+# Custom apps
+     "accounts",
+     "bookings",
 ]
 
 MIDDLEWARE = [
