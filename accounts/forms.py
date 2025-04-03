f4a55e62cb88b72d1874f3692b1a6db160fbdf8d
Cecilia-Banda <c.banda@alustudent.com>
2025-04-04 01:24:41 +0200

    ansible

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import logout
 from django.shortcuts import redirect, render
 from .forms import UserRegisterForm
 
