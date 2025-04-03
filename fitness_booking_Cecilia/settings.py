36334db9516bf33e49d54588b010755927a723b4
Cecilia-Banda <c.banda@alustudent.com>
2025-04-04 01:03:00 +0200

    ssh keys

--- fitness_booking_Cecilia/settings.py
+++ fitness_booking_Cecilia/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "fitness_booking_Cecilia",  # Main app
 # Custom apps
      "accounts",
      "bookings",
